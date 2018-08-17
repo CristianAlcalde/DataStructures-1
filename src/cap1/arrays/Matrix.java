9a6490f45306937cfa4d38db08ae5efc6c483fde
Juanjo <jquicenomarin@gmail.com>
2018-08-17 13:05:15 -0500

    create variables and getters and setters

--- src/cap1/arrays/Matrix.java
+++ src/cap1/arrays/Matrix.java
@@ -1,5 +1,26 @@
 package cap1.arrays;
 
 public class Matrix {
+	private int rows;
+	private int columns;
+	private int[] elements;
+	public int getRows() {
+		return rows;
+	}
+	public void setRows(int rows) {
+		this.rows = rows;
+	}
+	public int getColumns() {
+		return columns;
+	}
+	public void setColumns(int columns) {
+		this.columns = columns;
+	}
+	public int[] getElements() {
+		return elements;
+	}
+	public void setElements(int[] elements) {
+		this.elements = elements;
+	} 
 
 }
