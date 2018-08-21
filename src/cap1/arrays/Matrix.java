27cce69de86a38f09df2c10238a408f66c522d74
jdelgadoutp <j.delgado@utp.edu.co>
2018-08-20 19:20:33 -0500

    Inicio trabajo estructuras de datos.

--- src/cap1/arrays/Matrix.java
+++ src/cap1/arrays/Matrix.java
@@ -2,6 +2,10 @@ package cap1.arrays;
 
 public class Matrix {
 	// Step 1. Create attributes
+	private int sizerow;
+	private int sizerowcolumn;
+	private int[][] elements;
+	
 	// Step 1.1. Create Constructor
 	// Step 2. Create methods
 	// Step 2.1. Generate encapsulation methods
