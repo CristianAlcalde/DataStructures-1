0b89e967c8f750c7ae8acb11f6f727488fd66a32
Daniel Alarcón <daniel.alarcon@utp.edu.co>
2018-08-20 20:00:19 -0500

    Working on Vector CRUD Methods.

--- src/test/VectorTest.java
+++ src/test/VectorTest.java
@@ -10,7 +10,7 @@ import cap1.arrays.Vector;
 class VectorTest {
 
 	@Test
-	void testInsertElement() {
+	void test_InsertElement() {
 		// Step 1. instantiate the tested class using the constructor method
 		Vector testedVector = new Vector(10);
 		// Step 2. Create the expected value variable
@@ -23,7 +23,7 @@ class VectorTest {
 	}
 	
 	@Test
-	void testSearchElement() {
+	void test_SearchElement() {
 		// Step 1. instantiate the tested class using the constructor method
 		Vector testedVector = new Vector(10);
 		// Step 2. Create the expected value variable
@@ -34,5 +34,66 @@ class VectorTest {
 		// Step 4. Compare the expected versus actual values
 		assertEquals(expectedValue, actualValue);		
 	}
+	
+	@Test
+	void test_UpdateByPosition() {
+		// Step 1. instantiate the tested class using the constructor method
+		Vector testedVector = new Vector(10);
+		// Step 2. Create the initial and the expected value variable
+		int initialValue= 1976;
+		int expectedValue = 1996;
+		// Step 3. Insert any value that will be update. Add it in position 3
+		testedVector.insertElement(initialValue, 3);
+		// Step 4. Update the position with the expected value
+		int updatedValue = testedVector.updateElementByPosition(3, expectedValue);
+		// Step 4. Compare the expected versus actual values
+		assertEquals(expectedValue, updatedValue);		
+	}
+	
+	@Test
+	void test_UpdateByElement() {
+		// Step 1. instantiate the tested class using the constructor method
+		Vector testedVector = new Vector(10);
+		// Step 2. Create the initial and the expected value variable
+		int initialValue= 1976;
+		int expectedValue = 1996;
+		// Step 3. Insert any value that will be update. Add it in position 3
+		testedVector.insertElement(initialValue, 3);
+		// Step 4. Update the position with the expected value
+		int updatedValue = testedVector.updateElementByElement(initialValue, expectedValue);
+		// Step 4. Compare the expected versus actual values
+		assertEquals(expectedValue, updatedValue);		
+	}
 
+	@Test
+	void test_RemoveByPosition() {
+		// Step 1. instantiate the tested class using the constructor method
+		Vector testedVector = new Vector(10);
+		// Step 2. Create the initial and the expected value variable
+		int initialValue= 1976;
+		int expectedValue = 0;
+		// Step 3. Insert any value that will be update. Add it in position 3
+		testedVector.insertElement(initialValue, 3);
+		// Step 4. Update the position with the expected value
+		int removedValue = testedVector.removeElementByPosition(3);
+		// Step 4. Compare the expected versus actual values
+		assertEquals(expectedValue, removedValue);		
+	}
+	
+	@Test
+	void test_RemoveByElement() {
+		// Step 1. instantiate the tested class using the constructor method
+		Vector testedVector = new Vector(10);
+		// Step 2. Create the initial and the expected value variable
+		int initialValue= 1976;
+		int expectedValue = 0;
+		// Step 3. Insert any value that will be update. Add it in position 3
+		testedVector.insertElement(initialValue, 3);
+		// Step 4. Update the position with the expected value
+		int removedValue = testedVector.removeElementByElement(initialValue);
+		// Step 4. Compare the expected versus actual values
+		assertEquals(expectedValue, removedValue);		
+	}
+	
+	
 }
