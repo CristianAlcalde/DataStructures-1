9b1f18a1a9394631cd23b886cc0aa31b9454374c
Anibal Toro <anibal.toro@utp.edu.co>
2018-08-20 20:27:27 -0500

    inicio proyecto estructuras.

--- src/cap1/arrays/Matrix.java
+++ src/cap1/arrays/Matrix.java
@@ -2,6 +2,7 @@ package cap1.arrays;
 
 public class Matrix {
 	// Step 1. Create attributes
+	int matriz;
 	// Step 1.1. Create Constructor
 	// Step 2. Create methods
 	// Step 2.1. Generate encapsulation methods
