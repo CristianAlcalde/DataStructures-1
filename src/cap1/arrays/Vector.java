1c6cd450e9a2362a7fbbde5c60adab38cda3182c
enieto2018 <e.nieto@utp.edu.co>
2018-08-22 07:27:46 -0500

    prueba

--- src/cap1/arrays/Vector.java
+++ src/cap1/arrays/Vector.java
@@ -4,9 +4,10 @@ public class Vector {
 	// Step 1. Create attributes
 	private int size;
 	private int[] elements;
-	
+
+	// Step 1.1. Create Constructor
 	public Vector(int newSize) {
-		System.out.println("Creating vector with size="+newSize);
+		System.out.println("Creating vector with size=" + newSize);
 		this.size = newSize;
 		this.elements = new int[newSize];
 	}
@@ -28,21 +29,43 @@ public class Vector {
 	public void setElements(int[] elements) {
 		this.elements = elements;
 	}
+
 	// Step 2.2. Create CRUD methods
 	public int[] insertElement(int element, int position) {
-		this.elements[position] = element;		
+		this.elements[position] = element;
 		return this.elements;
 	}
-	
-	public int searchElement(int position) {
+
+	public int searchElementByPosition(int position) {
 		return this.elements[position];
 	}
 	
-	
-	
-	
-	
-	
-	
-	
+	public int searchElementByValue(int element) {
+		int result = -1;
+		// Recorrer el vector buscando el elemento a eliminar
+		for (int i = 0; i < elements.length; i++) {
+			// Si el elemento es encontrado reemplazarlo por 0
+			if (elements[i] == element) {
+				result = i;
+			}
+		}
+		return result;
+	}
+
+	public int[] updateElement(int newElement, int position) {
+		this.elements[position] = newElement;
+		return this.elements;
+	}
+
+	public int[] deleteElement(int element) {
+
+		// Recorrer el vector buscando el elemento a eliminar
+		for (int i = 0; i < elements.length; i++) {
+			// Si el elemento es encontrado reemplazarlo por 0
+			if (elements[i] == element) {
+				elements[i] = 0;
+			}
+		}
+		return this.elements;
+	}
 }
--- src/test/VectorTest.java
+++ src/test/VectorTest.java
@@ -15,13 +15,13 @@ class VectorTest {
 		Vector testedVector = new Vector(10);
 		// Step 2. Create the expected value variable
 		int[] expectedValue = new int[10];
-		expectedValue[0]=999;
+		expectedValue[0] = 999;
 		// Step 3. Obtain the actual value
 		int[] actualValue = testedVector.insertElement(999, 0);
 		// Step 4. Compare the expected versus actual values
 		assertArrayEquals(expectedValue, actualValue);
 	}
-	
+
 	@Test
 	void testSearchElement() {
 		// Step 1. instantiate the tested class using the constructor method
@@ -30,9 +30,36 @@ class VectorTest {
 		int expectedValue = 999;
 		testedVector.insertElement(expectedValue, 9);
 		// Step 3. Obtain the actual value
-		int actualValue = testedVector.searchElement(9);
+		int actualValue = testedVector.searchElementByPosition(9);
 		// Step 4. Compare the expected versus actual values
-		assertEquals(expectedValue, actualValue);		
+		assertEquals(expectedValue, actualValue);
 	}
 
+	@Test
+	void testUpdateElement() {
+		// Step 1. instantiate the tested class using the constructor method
+		Vector testedVector = new Vector(10);
+		// Step 2. Create the expected value variable
+		int[] expectedValue = new int[10];
+		expectedValue[0] = 888;
+		// Step 3. Obtain the actual value
+		int[] actualValue = testedVector.updateElement(888, 0);
+		// Step 4. Compare the expected versus actual values
+		assertArrayEquals(expectedValue, actualValue);
+	}
+
+	@Test
+	void testDeleteElement() {
+		// Step 1. instantiate the tested class using the constructor method
+		Vector testedVector = new Vector(10);
+		// Step 2. Create the expected value variable
+		int expectedValue = 999;
+		testedVector.insertElement(expectedValue, 9);
+		// Step 3. Obtain the actual value
+		int[] actualValue = testedVector.deleteElement(expectedValue);
+		// Step 4. Compare the expected versus actual values
+		int pos = testedVector.searchElementByValue(expectedValue);
+
+		assertEquals(-1, pos);
+	}
 }
