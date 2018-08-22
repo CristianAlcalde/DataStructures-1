1e76d786d6401e1ff74703f88517e8a052f49030
alejandroSoto <alejandro.soto@utp.edu.co>
2018-08-22 17:53:24 -0500

    atributos de la matriz

--- src/cap1/arrays/Matrix.java
+++ src/cap1/arrays/Matrix.java
@@ -2,6 +2,27 @@ package cap1.arrays;
 
 public class Matrix {
 	// Step 1. Create attributes
+	private int tamFila;
+	private int tamColumna;
+	
+	public void setTamFila(int tamFila) {
+		this.tamFila = tamFila;
+	}
+	
+	public int getTamFila() {
+		return tamFila;
+	}
+	
+	public void setTamColumna(int tamColumna) {
+		this.tamColumna= tamColumna;
+	}
+	
+	public int getTamColumna() {
+		return tamColumna;
+	}
+	
+	
+	
 	// Step 1.1. Create Constructor
 	// Step 2. Create methods
 	// Step 2.1. Generate encapsulation methods
