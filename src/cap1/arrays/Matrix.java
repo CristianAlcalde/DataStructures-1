76eccf4d865cf7f078a49ada18fd09c4b1032e33
Anibal Toro <anibal.toro@utp.edu.co>
2018-08-23 19:00:31 -0500

    inicio de matriz.

--- src/cap1/arrays/Matrix.java
+++ src/cap1/arrays/Matrix.java
@@ -2,9 +2,27 @@ package cap1.arrays;
 
 public class Matrix {
 	// Step 1. Create attributes
-	int matriz;
+	private int size;
+	private int [][] elements;
+	public Matrix(int newsize) {
+		
+	}
 	// Step 1.1. Create Constructor
+	
+	
 	// Step 2. Create methods
 	// Step 2.1. Generate encapsulation methods
+	public int getSize() {
+		return size;
+	}
+	public void setSize(int size) {
+		this.size = size;
+	}
+	public int [][] getElements() {
+		return elements;
+	}
+	public void setElements(int [][] elements) {
+		this.elements = elements;
+	}
 	// Step 2.2. Create CRUD methods
 }
