bc80b0c4d00b3ee39d5d0f286696d6c4a1f3c5f2
Dimas <dimas.hernandez@utp.edu.co>
2018-08-23 23:38:15 -0500

    Creación de los atributos,constructor y generar el encapsulamiento de los metodos.

--- src/cap1/arrays/Matrix.java
+++ src/cap1/arrays/Matrix.java
@@ -2,9 +2,50 @@ package cap1.arrays;
 
 public class Matrix {
 	// Step 1. Create attributes
+
+	private int files;
+	private int columns;
+	private int elements[][];
+
 	// Step 1.1. Create Constructor
+	public Matrix(int newFiles, int newColumns) {
+		System.out.println("Creating matrix with size = " + newFiles + newColumns);
+		this.files = newFiles;
+		this.columns = newColumns;
+		this.elements = new int[newFiles][newColumns];
+	}
+
 	// Step 2. Create methods
 	// Step 2.1. Generate encapsulation methods
-	// Step 2.2. Create CRUD methods  
+
+	public int getFiles() {
+		return files;
+	}
+
+	public void setFiles(int files) {
+		this.files = files;
+	}
+
+	public int getColumns() {
+		return columns;
+	}
+
+	public void setColumns(int columns) {
+		this.columns = columns;
+	}
+
+	public int[][] getelements() {
+		return elements;
+	}
+
+	public void setElements(int[][] elements) {
+		this.elements = elements;
+	}
+
+	// Step 2.2. Create CRUD methods
 	
+	
+	
+	
+
 }
