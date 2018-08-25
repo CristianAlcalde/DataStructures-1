567c89342d394938c92a85e51b9e4cd698b1c18a
Anibal Toro <anibal.toro@utp.edu.co>
2018-08-24 22:27:43 -0500

    insertElements.

--- src/cap1/arrays/Matrix.java
+++ src/cap1/arrays/Matrix.java
@@ -3,10 +3,13 @@ package cap1.arrays;
 public class Matrix {
 	// Step 1. Create attributes
 	private int size;
-	private int [][] elements;
+	private int[][] elements;
 	
 	public Matrix(int newsize,int newsize1) {
 	// Step 1.1. Create Constructor
+		System.out.println("Creating Matrix with size=" + newsize + newsize1);
+		this.size = newsize;
+		this.elements = new int[newsize][newsize1];
 	}
 	// Step 2. Create methods
 	// Step 2.1. Generate encapsulation methods
@@ -16,15 +19,16 @@ public class Matrix {
 	public void setSize(int size) {
 		this.size = size;
 	}
-	public int [][] getElements() {
+	public int[][] getElements() {
 		return elements;
 	}
-	public void setElements(int [][] elements) {
+	public void setElements(int[][] elements) {
 		this.elements = elements;
 	}
 	// Step 2.2. Create CRUD methods
-	public int insertElements(int rows,int columns) {
+	public int[][] insertElements(int rows,int columns, int elements) {
+		this.elements[rows][columns]=elements;
 		
-		return 0;
+		return this.elements;
 	}
 }
--- src/test/MatrixTest.java
+++ src/test/MatrixTest.java
@@ -1,14 +1,26 @@
 package test;
 
+import static org.junit.Assert.assertArrayEquals;
 import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
 
+import cap1.arrays.Matrix;
+import cap1.arrays.Vector;
+
 class MatrixTest {
 
 	@Test
-	void test() {
-		//fail("Not yet implemented");
+	void testInsertElement() {
+		// Step 1. instantiate the tested class using the constructor method
+		Matrix testedMatrix = new Matrix(10 , 10);
+		// Step 2. Create the expected value variable
+		int[][] expectedValue = new int[10] [10];
+		expectedValue[5][3] =588;
+		// Step 3. Obtain the actual value
+		int[][] actualValue = testedMatrix.insertElements(5,3,588);
+		// Step 4. Compare the expected versus actual values
+		assertArrayEquals(expectedValue, actualValue);
 	}
 
 }
