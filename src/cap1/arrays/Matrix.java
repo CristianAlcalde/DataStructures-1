1fbe0ae0ae51f45e57671f9a899d2a2a2c8395b4
Leandro Londoño <leandro.ciro@utp.edu.co>
2018-08-26 12:29:30 -0500

    Restableciendo rama

--- src/cap1/arrays/Matrix.java
+++ src/cap1/arrays/Matrix.java
@@ -2,6 +2,10 @@ package cap1.arrays;
 
 public class Matrix {
 	// Step 1. Create attributes
+	private int sizeLines;
+	private int sizeColumns;
+	private int[] elements;
+	
 	// Step 1.1. Create Constructor
 	// Step 2. Create methods
 	// Step 2.1. Generate encapsulation methods
