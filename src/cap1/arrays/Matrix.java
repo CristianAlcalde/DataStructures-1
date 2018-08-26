4a9f3976d7bffc86f6d54dae8bd34afc6b1fe809
Anibal Toro <anibal.toro@utp.edu.co>
2018-08-26 15:43:31 -0500

    searchElements.

--- src/cap1/arrays/Matrix.java
+++ src/cap1/arrays/Matrix.java
@@ -4,6 +4,7 @@ public class Matrix {
 	// Step 1. Create attributes
 	private int size;
 	private int[][] elements;
+	int [][] matriz={{7,14,8,3},{6,19,7,2},{3,13,4,1}};
 	
 	public Matrix(int newsize,int newsize1) {
 	// Step 1.1. Create Constructor
@@ -30,5 +31,15 @@ public class Matrix {
 		this.elements[rows][columns]=elements;
 		
 		return this.elements;
+	}
+	public int searchElements(int element) {
+		int result=0;
+		for (int x =0; x <elements.length;x++) {
+			for(int y=0; y <elements.length; y++) {
+				if(elements[x][y]==element);
+				result=element;
+			}
+		}
+		return result;
 	}
 }
--- src/test/MatrixTest.java
+++ src/test/MatrixTest.java
@@ -13,14 +13,25 @@ class MatrixTest {
 	@Test
 	void testInsertElement() {
 		// Step 1. instantiate the tested class using the constructor method
-		Matrix testedMatrix = new Matrix(10 , 10);
+		Matrix testedMatrix = new Matrix(4 , 4);
 		// Step 2. Create the expected value variable
-		int[][] expectedValue = new int[10] [10];
-		expectedValue[5][3] =588;
+		int[][] expectedValue = new int[4] [4];
+		expectedValue[2][3] =5;
 		// Step 3. Obtain the actual value
-		int[][] actualValue = testedMatrix.insertElements(5,3,588);
+		int[][] actualValue = testedMatrix.insertElements(2,3,5);
 		// Step 4. Compare the expected versus actual values
 		assertArrayEquals(expectedValue, actualValue);
+	}
+	void testsearchElement() {
+		// Step 1. instantiate the tested class using the constructor method
+		Matrix testedMatrix = new Matrix(4 , 4);
+		// Step 2. Create the expected value variable
+		int[][] expectedValue = new int[4] [4];
+		expectedValue[2][3] =5;
+		// Step 3. Obtain the actual value
+		int actualValue = testedMatrix.searchElements(5);
+		// Step 4. Compare the expected versus actual values
+		assertEquals(expectedValue, actualValue);
 	}
 
 }
