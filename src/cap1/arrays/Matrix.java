6b925ab45179123f1438b1c271fcdb80de4ceaa6
Leonardo Baquero <leonardo.baquero@utp.edu.co>
2018-08-26 18:46:15 -0500

    Creando atributos de la matriz.

--- src/cap1/arrays/Matrix.java
+++ src/cap1/arrays/Matrix.java
@@ -2,8 +2,19 @@ package cap1.arrays;
 
 public class Matrix {
 	// Step 1. Create attributes
+	private int size;
+	private int[] elementsFilas;
+	private int[] elementsColumnas;
+	
 	// Step 1.1. Create Constructor
+	public Matrix(int newSize) {
+		System.out.println("Creating matrix with size=" + newSize);
+		this.size = newSize;
+		this.elementsFilas = new int[newSize];
+		this.elementsColumnas = new int[newSize];
+		
 	// Step 2. Create methods
+		
 	// Step 2.1. Generate encapsulation methods
 	// Step 2.2. Create CRUD methods
 }
