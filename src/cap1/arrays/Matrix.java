fa2249e425508129ab0be1d684d661598bc9abc4
Leonardo Baquero <leonardo.baquero@utp.edu.co>
2018-08-26 23:04:33 -0500

    creando metodo CRUD.

--- src/cap1/arrays/Matrix.java
+++ src/cap1/arrays/Matrix.java
@@ -2,83 +2,36 @@ package cap1.arrays;
 
 public class Matrix {
 	// Step 1. Create attributes
-	
 	private int size;
 	private int[][] element;
 	
+	
+	public Matrix(int newsizex, int newsizey) {
+		this.size = size;
+		this.element = element;
+	}
 	// Step 2. Create methods
 	// Step 2.1. Generate encapsulation methods
-	
 	public int getSize() {
 		return size;
 	}
-
 	public void setSize(int size) {
 		this.size = size;
 	}
-
 	public int[][] getElement() {
 		return element;
 	}
-
 	public void setElement(int[][] element) {
 		this.element = element;
 	}
 	
-	// Step 2.2. Create CRUD methods	
-	
-	public int[][] insertElement(int[] position, intelementB)
-	
-	
-	/*				
-
-		// Step 2.2. Create CRUD methods
-		public int[] insertElement(int element, int position) {
-			this.elements[position] = element;
-			return this.elements;
-		}
-
-		public int searchElementByPosition(int position) {
-			return this.elements[position];
-		}
-		
-		public int searchElementByValue(int element) {
-			int result = -1;
-			// Recorrer el vector buscando el elemento a eliminar
-			for (int i = 0; i < elements.length; i++) {
-				// Si el elemento es encontrado reemplazarlo por 0
-				if (elements[i] == element) {
-					result = i;
-				}
-			}
-			return result;
-		}
-
-		public int[] updateElement(int newElement, int position) {
-			this.elements[position] = newElement;
-			return this.elements;
-		}
-
-		public int[] deleteElement(int element) {
-
-			// Recorrer el vector buscando el elemento a eliminar
-			for (int i = 0; i < elements.length; i++) {
-				// Si el elemento es encontrado reemplazarlo por 0
-				if (elements[i] == element) {
-					elements[i] = 0;
-				}
-			}
-			return this.elements;
-		}
+	// Step 2.2. Create CRUD methods
+	public int [][] insertElement(int element1 ,int positionA,int positionB) {
+		this.element[positionA][positionB] = element1;
+		return this.element;
 	}
-
+			
+	
 }
-
-*/
-
-}
-
-
-
 	
 
