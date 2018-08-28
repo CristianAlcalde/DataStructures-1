34c826b4edfb839f2d126dd890a4c8a010e6b9f8
Maicol <maiikol_96@utp.edu.co>
2018-08-28 12:03:43 -0500

    Creamos el constructor

--- src/cap1/arrays/Matrix.java
+++ src/cap1/arrays/Matrix.java
@@ -2,7 +2,18 @@ package cap1.arrays;
 
 public class Matrix {
 	// Step 1. Create attributes
+	
+	private int filas, columnas;
+	private int [][] elementos;
+	
 	// Step 1.1. Create Constructor
+	
+	public Matrix(int newFilas, int NewColumnas) {
+		System.out.println("Creating matrix with filas and columnas=" + newFilas, newColumnas);
+		this.filas = newFilas;
+		this.columnas = newColumnas
+		this.elementos = new int[newFilas][newColumnas];
+	
 	// Step 2. Create methods
 	// Step 2.1. Generate encapsulation methods
 	// Step 2.2. Create CRUD methods
