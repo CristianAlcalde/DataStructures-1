4c079d33dfde1f48a4c75aba5ac9614e09660424
alejandroSoto <alejandro.soto@utp.edu.co>
2018-08-28 18:45:51 -0500

    Metodo insertar matriz

--- src/cap1/arrays/Matrix.java
+++ src/cap1/arrays/Matrix.java
@@ -4,30 +4,48 @@ public class Matrix {
 	// Step 1. Create attributes
 	private int tamFila;
 	private int tamColumna;
+	private int[][] matriz;
 	
 	// Step 1.1. Create Constructor
-	public int matrix[][];
-		
-	
-	// Step 2. Create methods
-	public void setTamFila(int tamFila) {
-		this.tamFila = tamFila;
+	public Matrix(int nuevoTamFila, int nuevoTamColumna) {
+		System.out.println("Creando matriz con tamaño=" + nuevoTamFila + "*" + nuevoTamColumna);
+		this.tamFila = nuevoTamFila;
+		this.tamColumna = nuevoTamColumna;
+		this.matriz = new int[nuevoTamFila][nuevoTamColumna];
 	}
 	
+	// Step 2. Create methods
+	// Step 2.1. Generate encapsulation methods
 	public int getTamFila() {
 		return tamFila;
 	}
 	
-	public void setTamColumna(int tamColumna) {
-		this.tamColumna= tamColumna;
+	public void setTamFila(int tamFila) {
+		this.tamFila = tamFila;
 	}
-	
+			
 	public int getTamColumna() {
 		return tamColumna;
 	}
 	
-	// Step 2.1. Generate encapsulation methods
+	public void setTamColumna(int tamColumna) {
+		this.tamColumna = tamColumna;
+	}
+	
+	public int[][] getMatriz() {
+		return matriz;
+	}
+		
+	public void setMatriz(int[][] matriz) {
+		this.matriz= matriz;
+	}
+		
 	// Step 2.2. Create CRUD methods
+	public int[][] insertarElementos(int elemento, int posFila, int posColumna){
+		this.matriz[posFila][posColumna] = elemento;
+		return this.matriz;
+	}
+	
 	
 	
 }
--- src/test/MatrixTest.java
+++ src/test/MatrixTest.java
@@ -1,14 +1,27 @@
 package test;
 
+import static org.junit.Assert.assertArrayEquals;
 import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
 
+import cap1.arrays.Matrix;
+import cap1.arrays.Vector;
+
 class MatrixTest {
 
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	void testInsertarElemento() {
+
+		// Step 1. Inicializar la clase test usando el metodo constructor
+		Matrix testMatriz = new Matrix(10, 10); // dar tamaño a la matriz
+		// Step 2. Crear la variable del valor esperado
+		int[][] resultadoEsperado = new int[10][10]; // tambien es matriz
+		resultadoEsperado[3][5]=28; // insertar elemento en la matriz (posicionFila, posicionColumna, (28)
+		// Step 3. Obtain the actual value
+		int[][] resultadoObtenido = testMatriz.insertarElementos(28, 3, 5);
+		// Step 4. Compara el valor esperado con el valor obtenido
+		assertArrayEquals(resultadoEsperado, resultadoObtenido);
 	}
 
 }
