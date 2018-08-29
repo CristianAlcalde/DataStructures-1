9c17244fe3690435116c38cb39573503d8fa5bb0
Maicol <maiikol_96@utp.edu.co>
2018-08-29 13:41:14 -0500

    insertar elemento test

--- src/test/MatrixTest.java
+++ src/test/MatrixTest.java
@@ -8,7 +8,27 @@ class MatrixTest {
 
 	@Test
 	void test() {
-		fail("Not yet implemented");
+		
+		void testInsertarElementos() {
+			// Step 1. instantiate the tested class using the constructor method
+			Matrix testedMatrix = new Matrix[10][10];
+			// Step 2. Create the expected value variable
+			int[][] expectedValue = new int[10][10];
+			expectedValue[0][0] = 999;
+			// Step 3. Obtain the actual value
+			int[][] actualValue = testedMatrix.insertElement(999, [0][0]);
+			// Step 4. Compare the expected versus actual values
+			assertArrayEquals(expectedValue, actualValue);
+		}
+		
+		
+		
+		
+		
+		
+		
+		
+		
 	}
 
 }
