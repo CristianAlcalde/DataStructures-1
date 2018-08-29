68da60fc0f87875694518c77f402117d0b57718e
alejandroSoto <alejandro.soto@utp.edu.co>
2018-08-29 17:37:10 -0500

    Matriz ordenada

--- src/cap1/arrays/Matrix.java
+++ src/cap1/arrays/Matrix.java
@@ -6,7 +6,7 @@ public class Matrix {
 	private int tamColumna;
 	private int[][] matriz;
 	int[] vec;
-	
+
 	// Step 1.1. Create Constructor
 	public Matrix(int nuevoTamFila, int nuevoTamColumna) {
 		System.out.println("Creando matriz con tamaño=" + nuevoTamFila + "*" + nuevoTamColumna);
@@ -15,97 +15,96 @@ public class Matrix {
 		this.matriz = new int[nuevoTamFila][nuevoTamColumna];
 		this.vec = new int[nuevoTamFila];
 	}
-	
+
 	// Step 2. Create methods
 	// Step 2.1. Generate encapsulation methods
 	public int getTamFila() {
 		return tamFila;
 	}
-	
+
 	public void setTamFila(int tamFila) {
 		this.tamFila = tamFila;
 	}
-			
+
 	public int getTamColumna() {
 		return tamColumna;
 	}
-	
+
 	public void setTamColumna(int tamColumna) {
 		this.tamColumna = tamColumna;
 	}
-	
+
 	public int[][] getMatriz() {
 		return matriz;
 	}
-		
+
 	public void setMatriz(int[][] matriz) {
-		this.matriz= matriz;
+		this.matriz = matriz;
 	}
-		
+
 	// Step 2.2. Create CRUD methods
-	
+
 	// Método Insertar
-	
-	public int[][] insertarElementos(int elemento, int posFila, int posColumna){
+
+	public int[][] insertarElementos(int elemento, int posFila, int posColumna) {
 		this.matriz[posFila][posColumna] = elemento;
 		return this.matriz;
 	}
-	
+
 	// Metodo Listar
-	
-	public void listarElementos(){
-		for(int i=0; i<matriz.length; i++) {
-			for(int j=0; j<matriz[i].length; j++) {
+
+	public void listarElementos() {
+		for (int i = 0; i < matriz.length; i++) {
+			for (int j = 0; j < matriz[i].length; j++) {
 				System.out.print(matriz[i][j] + " ");
 			}
 			System.out.println("");
-			
+
 		}
-		
+
 	}
-	
+
 	// Metodo Buscar
-		
-	public boolean buscarElemento(int elementoBuscado){
+
+	public boolean buscarElemento(int elementoBuscado) {
 		int resultado = -1;
-		for(int i=0; i<matriz.length; i++) {
-			for(int j=0; j<matriz[i].length; j++) {
-				if(matriz[i][j]==elementoBuscado) {
-					resultado=elementoBuscado;
-					
+		for (int i = 0; i < matriz.length; i++) {
+			for (int j = 0; j < matriz[i].length; j++) {
+				if (matriz[i][j] == elementoBuscado) {
+					resultado = elementoBuscado;
+
 				}
 			}
 		}
-		if(resultado!=-1) {
-			return true;}
-		else { return false;}
-		
+		if (resultado != -1) {
+			return true;
+		} else {
+			return false;
+		}
+
 	}
-	
-	
+
 	// Metodo Eliminar
-	
-		public boolean eliminarElemento(int elemento){
-			boolean resultado=false;
-			for(int i=0; i<matriz.length; i++) {
-				for(int j=0; j<matriz[i].length; j++) {
-					if(matriz[i][j]==elemento) {
-						matriz[i][j]=0;
-						resultado=true;
-					}
+
+	public boolean eliminarElemento(int elemento) {
+		boolean resultado = false;
+		for (int i = 0; i < matriz.length; i++) {
+			for (int j = 0; j < matriz[i].length; j++) {
+				if (matriz[i][j] == elemento) {
+					matriz[i][j] = 0;
+					resultado = true;
 				}
 			}
-			return resultado;
-			
 		}
-	
+		return resultado;
+
+	}
+
 	// Método Actualizar
-		
-		public int[][] actualizarElemento(int nuevoElemento, int fila, int columna) {
-			this.matriz[fila][columna] = nuevoElemento;
-			return this.matriz;
-		}
-	
-	
-	
+
+	public int[][] actualizarElemento(int nuevoElemento, int fila, int columna) {
+		this.matriz[fila][columna] = nuevoElemento;
+		return this.matriz;
+	}
+
 }
