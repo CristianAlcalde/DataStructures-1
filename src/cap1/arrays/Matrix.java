21828c29defd198151a8418cae73d71e85fd69dd
Leonardo Baquero <leonardo.baquero@utp.edu.co>
2018-08-29 00:11:43 -0500

    "test de insertar elemento y prueba de uso de commits directamente desde Eclipse."

--- src/cap1/arrays/Matrix.java
+++ src/cap1/arrays/Matrix.java
@@ -3,13 +3,15 @@ package cap1.arrays;
 public class Matrix {
 	// Step 1. Create attributes
 	private int size;
-	private int[][] element;
+	private int[][] elements;
 	
-	
-	public Matrix(int newsizex, int newsizey) {
+	// Step 1.1. Create Constructor
+	public Matrix(int newsizex, int newsizey) {	
+		System.out.println("Creating vector with size=");
 		this.size = size;
-		this.element = element;
+		this.elements = new int[newsizex][newsizey];
 	}
+	
 	// Step 2. Create methods
 	// Step 2.1. Generate encapsulation methods
 	public int getSize() {
@@ -19,19 +21,16 @@ public class Matrix {
 		this.size = size;
 	}
 	public int[][] getElement() {
-		return element;
+		return elements;
 	}
 	public void setElement(int[][] element) {
-		this.element = element;
+		this.elements = element;
 	}
 	
 	// Step 2.2. Create CRUD methods
 	public int [][] insertElement(int element1 ,int positionA,int positionB) {
-		this.element[positionA][positionB] = element1;
-		return this.element;
+		this.elements[positionA][positionB] = element1;
+		return this.elements;
 	}
-			
-	
+				
 }
-	
-
--- src/test/MatrixTest.java
+++ src/test/MatrixTest.java
@@ -1,14 +1,47 @@
 package test;
 
-import static org.junit.jupiter.api.Assertions.*;
-
+import static org.junit.Assert.assertArrayEquals;
 import org.junit.jupiter.api.Test;
+import cap1.arrays.Matrix;
 
 class MatrixTest {
 
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	void testInsertElement() {
+		// Step 1. instantiate the tested class using the constructor method
+		Matrix testMatrix = new Matrix(3,3);
+		// Step 2. Create the expected value variable
+		int[][] expectedValue = new int[3][3];
+		expectedValue[2][1] = 11;
+		// Step 3. Obtain the actual value
+		int[][] actualValue = testMatrix.insertElement(11, 2, 1);	
+		// Step 4. Compare the expected versus actual values		
+		assertArrayEquals(expectedValue, actualValue);
 	}
-
+/*	@Test
+	void testSearchElement() {
+		// Step 1. instantiate the tested class using the constructor method
+		Matrix testMatrix = new Matrix(3,3);
+		// Step 2. Create the expected value variable
+		int expectedValue = 11;
+		testMatrix.insertElement(expectedValue, 2, 2);		
+		// Step 3. Obtain the actual value
+		int actualValue = testMatrix.searchElementByPosition(2, 2);	
+		// Step 4. Compare the expected versus actual values		
+		assertEquals(expectedValue, actualValue);
+	}
+	@Test
+	void testUpdateElement() {
+		// Step 1. instantiate the tested class using the constructor method
+		Matrix testMartix = new Matrix (3,3);
+		// Step 2. Create the expected value variable
+		int[][] expectedValue = new int[3][3];
+		expectedValue[0][0] = 11;
+		// Step 3. Obtain the actual value
+		int[][] actualValue = testMartix.updateElement(11, 0, 0);
+		// Step 4. Compare the expected versus actual values
+		assertArrayEquals(expectedValue, actualValue);
+	}
+*/
 }
+	
