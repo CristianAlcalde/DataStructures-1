1c6ac11b2efe286dd3bb41b1bc989221155c332f
Juannauta <juandavidvillaloaiza@utp.edu.co>
2018-08-29 15:52:02 -0500

    Creacion de clase matrix, metodos, respectivo test

--- src/cap1/arrays/Matrix.java
+++ src/cap1/arrays/Matrix.java
@@ -1,9 +1,72 @@
 package cap1.arrays;
 
 public class Matrix {
-	// Step 1. Create attributes
-	// Step 1.1. Create Constructor
-	// Step 2. Create methods
-	// Step 2.1. Generate encapsulation methods
-	// Step 2.2. Create CRUD methods
+	// Step 1. Crear atributos
+	private int size;
+	private int[] elements;
+	
+	// Step 1.1. Crea el contructor
+	public Matrix(int newSize) {
+		System.out.println("Creating matrix with size=" + newSize);
+		this.size = newSize;
+		this.elements = new int[newSize];
+	}
+	// Step 2. crea methods
+	// Step 2.1. genera el encapsulamiento de los metodos
+
+	public int getSize() {
+		return size;
+	}
+
+	public void setSize(int size) {
+		this.size = size;
+	}
+
+	public int[] getElements() {
+		return elements;
+	}
+
+	public void setElements(int[] elements) {
+		this.elements = elements;
+	}
+		
+	// Step 2.2. Crea CRUD y los methods+
+
+	public int[] insertElement(int element, int position) {
+		this.elements[position] = element;
+		return this.elements;
+	}
+
+	public int searchElementByPosition(int position) {
+		return this.elements[position];
+	}
+	
+	public int searchElementByValue(int element) {
+		int result = -1;
+		// Recorrer la matris, buscando el elemento a eliminar
+		for (int i = 0; i < elements.length; i++) {
+			// Si el elemento es encontrado reemplazarlo por 0
+			if (elements[i] == element) {
+				result = i;
+			}
+		}
+		return result;
+	}
+
+	public int[] updateElement(int newElement, int position) {
+		this.elements[position] = newElement;
+		return this.elements;
+	}
+
+	public int[] deleteElement(int element) {
+
+		// Recorrer la Matrix buscando el elemento a eliminar
+		for (int i = 0; i < elements.length; i++) {
+			// Si el elemento es encontrado reemplazarlo por 0
+			if (elements[i] == element) {
+				elements[i] = 0;
+			}
+		}
+		return this.elements;
+	}
 }
--- src/test/MatrixTest.java
+++ src/test/MatrixTest.java
@@ -1,14 +1,66 @@
 package test;
 
+import static org.junit.Assert.assertArrayEquals;
+import static org.junit.Assert.assertEquals;
 import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
 
+import cap1.arrays.Matrix;
+
 class MatrixTest {
 
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	void testInsertElement() {
+		// Step 1. Instanciamos el dato a testear usando los metodos creados
+		Matrix testedMatrix = new Matrix(77);
+		// Step 2. Creamos el dato a esperar
+		int[] expectedValue = new int[77];
+		expectedValue[0] = 100;
+		// Step 3. Obtiene el valor actual
+		int[] actualValue = testedMatrix.insertElement(100, 0);
+		// Step 4. Compara el valor actual con el esperado
+		assertArrayEquals(expectedValue, actualValue);
 	}
 
+	@Test
+	void testSearchElement() {
+		// Step 1. Instanciamos el dato a testear usando los metodos creados
+		Matrix testedMatrix = new Matrix(10);
+		// Step 2. Creamos el dato a esperar
+		int expectedValue = 999;
+		testedMatrix.insertElement(expectedValue, 9);
+		// Step 3. Obtiene el valor actual
+		int actualValue = testedMatrix.searchElementByPosition(9);
+		// Step 4. Compara el valor actual con el esperado
+		assertEquals(expectedValue, actualValue);
+	}
+
+	@Test
+	void testUpdateElement() {
+		// Step 1. Instanciamos el dato a testear usando los metodos creados
+		Matrix testedMatrix = new Matrix(10);
+		// Step 2. Creamos el dato a esperar
+		int[] expectedValue = new int[10];
+		expectedValue[0] = 888;
+		// Step 3. Obtiene el valor actual
+		int[] actualValue = testedMatrix.updateElement(888, 0);
+		// Step 4. Compara el valor actual con el esperado
+		assertArrayEquals(expectedValue, actualValue);
+	}
+
+	@Test
+	void testDeleteElement() {
+		// Step 1. Instanciamos el dato a testear usando los metodos creados
+		Matrix testedMatrix = new Matrix(10);
+		// Step 2. Creamos el dato a esperar
+		int expectedValue = 999;
+		testedMatrix.insertElement(expectedValue, 9);
+		// Step 3. Obtiene el valor actual
+		int[] actualValue = testedMatrix.deleteElement(expectedValue);
+		// Step 4. Compara el valor actual con el esperado
+		int pos = testedMatrix.searchElementByValue(expectedValue);
+
+		assertEquals(-1, pos);
+	}
 }
