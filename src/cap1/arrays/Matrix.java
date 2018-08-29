cb59cf6368883c39a54460cbbc2c06c2119faa97
willingtonortiz <w.ortiz@utp.edu.co>
2018-08-28 22:15:04 -0500

    test insertando elemento

--- src/cap1/arrays/Matrix.java
+++ src/cap1/arrays/Matrix.java
@@ -2,8 +2,67 @@ package cap1.arrays;
 
 public class Matrix {
 	// Step 1. Create attributes
+	private int file;
+	private int column;
+	private int elements[][];
+	
 	// Step 1.1. Create Constructor
+	public Matrix(int newFile,int newColumn) {
+		System.out.println("Creating matrix with elements=" + newFile + newColumn);
+		this.file = newFile;
+		this.column = newColumn;
+		this.elements = new int[newFile][newColumn];
+	}
 	// Step 2. Create methods
 	// Step 2.1. Generate encapsulation methods
+
+	public int getFile() {
+		return file;
+	}
+
+	public void setFile(int file) {
+		this.file = file;
+	}
+
+	public int getColumn() {
+		return column;
+	}
+
+	public void setColumn(int column) {
+		this.column = column;
+	}
+
+	public int[][] getElements() {
+		return elements;
+	}
+
+	public void setElements(int[][] elements) {
+		this.elements = elements;
+	}
+	
 	// Step 2.2. Create CRUD methods
-}
+	
+	public int [][] insertElement(int element, int positionFile, int positionColumn){
+		this.elements[positionFile][positionColumn] = element;
+		return this.elements;
+			
+	}
+	
+	public int searchElementbyPosition(int positionFile, int positionColumn) {
+		return this.elements[positionFile][positionColumn];
+	}
+	
+	public int[] searchElementbyValue(int value) {
+		int[] position =new int [2];
+		for (int i = 0; i < elements.length; i++) {
+			for (int j = 0; j < elements[i].length; j++) {
+				if (elements[i][j] ==value) {
+					position[0] = i;
+					position[1] = j;
+				}
+			}
+		}
+		return position;
+	}
+	}
+
--- src/test/MatrixTest.java
+++ src/test/MatrixTest.java
@@ -1,14 +1,27 @@
 package test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.Assert.assertArrayEquals;
+import static org.junit.Assert.assertEquals;
 
 import org.junit.jupiter.api.Test;
+
+import cap1.arrays.Matrix;
 
 class MatrixTest {
 
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	void testInsertElement() {
+		// Step 1. instantiate the tested class using the constructor method
+		Matrix testedMatrix = new Matrix(5, 5);
+		// Step 2. Create the expected value variable
+		int[][] expectedValue = new int[5][5];
+		expectedValue[0][0] = 300;
+		// Step 3. Obtain the actual value
+		int[][] actualValue = testedMatrix.insertElement(300, 0, 0);
+		// Step 4. Compare the expected versus actual values
+		assertArrayEquals(expectedValue, actualValue);
 	}
+	
+	
 
 }
