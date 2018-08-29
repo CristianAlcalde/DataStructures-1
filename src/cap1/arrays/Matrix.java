901bcea5ae22861b0899386b52a8b67935b0d9e0
enieto2018 <e.nieto@utp.edu.co>
2018-08-28 23:19:56 -0500

    Trabajo Matriz

--- src/cap1/arrays/Matrix.java
+++ src/cap1/arrays/Matrix.java
@@ -1,5 +1,94 @@
 package cap1.arrays;
 
 public class Matrix {
+	int filas;
+	int columnas;
+	int [][]elemnts;
+	
+	public Matrix(int filas, int columnas) {
+		System.out.print("Tamaño Matriz "+ filas+ " * " + columnas);
+		this.filas=filas;
+		this.columnas=columnas;
+		this.elemnts= new int[filas][columnas];
+	}
 
+
+	public int getFilas() {
+		return filas;
+	}
+
+
+	public void setFilas(int filas) {
+		this.filas = filas;
+	}
+
+
+	public int getColumnas() {
+		return columnas;
+	}
+
+
+	public void setColumnas(int columnas) {
+		this.columnas = columnas;
+	}
+
+
+	public int[][] getElemnts() {
+		return elemnts;
+	}
+
+
+	public void setElemnts(int[][] elemnts) {
+		this.elemnts = elemnts;
+	}
+	public int[][] insertarMatriz(int elemnts, int filas, int columnas) {
+		this.elemnts[filas][columnas]=elemnts;
+		return this.elemnts;
+	}
+	public int searchElementByPosition(int filas, int columnas) {
+		return this.elemnts[filas][columnas];
+	}
+	
+	public int searchElementByValue(int element) {
+		int result = -1;
+		// Recorrer el vector buscando el elemento a eliminar
+		for (int i = 0; i < elemnts.length; i++) {
+			for (int j = 0; j < elemnts.length; j++) {
+			// Si el elemento es encontrado reemplazarlo por 0
+				if (elemnts[i][j] ==element) {
+					result = i;
+				}
+			}
+		}
+		return result;
+	}
+	
+	public int[][] updateElement(int newElement,int filas, int columnas) {
+		this.elemnts[filas][columnas] = newElement;
+		return this.elemnts;
+	}
+	
+	public int[][] deleteElement(int element) {
+
+		// Recorrer el vector buscando el elemento a eliminar
+		for (int i = 0; i < elemnts.length; i++) {
+			for (int j = 0; j < elemnts.length; j++) {
+			// Si el elemento es encontrado reemplazarlo por 0
+				if (elemnts[i][j] == element) {
+					elemnts[i][j] = 0;
+				}
+			}
+		}
+		return this.elemnts;
+	}
+	public void imprimirMatriz() {
+
+	    for (int i = 0; i < filas; i++) {
+			for (int j = 0; j < columnas; j++) {
+	           System.out.print(elemnts[i][j]);
+
+	        }
+
+	    }
+	}
 }
--- src/test/MatrixTest.java
+++ src/test/MatrixTest.java
@@ -1,14 +1,70 @@
 package test;
 
+import static org.junit.Assert.assertArrayEquals;
+import static org.junit.Assert.assertEquals;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
 
+import cap1.arrays.Matrix;
+import cap1.arrays.Vector;
+
 class MatrixTest {
 
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	void testInsertar() {
+		//Step 1. instantiate the tested class using the constructor method
+		Matrix PruebaTamaño =new Matrix  (3,3);
+		// Step 2. Create the expected value variable
+		int [][] EsperaTamaño =new int[3][3];
+		EsperaTamaño[0][0]=4;
+		// Step 3. Obtain the actual value
+		int[][] ObtenerDato=PruebaTamaño.insertarMatriz(4, 0, 0);
+		// Step 4. Compare the expected versus actual values
+		assertArrayEquals(EsperaTamaño, ObtenerDato);
+			
+	}
+	@Test
+	void testSearchElement() {
+		// Step 1. instantiate the tested class using the constructor method
+		Matrix PruebaTamaño =new Matrix  (3,3);
+		// Step 2. Create the expected value variable
+		int EsperaTamaño = 4;
+		PruebaTamaño.insertarMatriz(EsperaTamaño, 1, 1);
+		// Step 3. Obtain the actual value
+		int ObtenerDato = PruebaTamaño.searchElementByPosition(1,1);
+		// Step 4. Compare the expected versus actual values
+		assertEquals(EsperaTamaño, ObtenerDato);
+	}
+	@Test
+	void testUpdateElement() {
+		// Step 1. instantiate the tested class using the constructor method
+		Matrix PruebaTamaño = new Matrix  (3,3);
+		// Step 2. Create the expected value variable
+		int [][] EsperaTamaño =new int[3][3];
+		EsperaTamaño[0][0]=6;
+		// Step 3. Obtain the actual value
+		int[][] ObtenerDato = PruebaTamaño.updateElement(6, 0, 0);
+		// Step 4. Compare the expected versus actual values
+		assertArrayEquals(EsperaTamaño, ObtenerDato);
+	}
+	@Test
+	void testDeleteElement() {
+		// Step 1. instantiate the tested class using the constructor method
+		Matrix PruebaTamaño = new Matrix  (3,3);
+		// Step 2. Create the expected value variable
+		int EsperaTamaño = 4;
+		PruebaTamaño.insertarMatriz(EsperaTamaño,1, 1);
+		// Step 3. Obtain the actual value
+		int[][] ObtenerDato = PruebaTamaño.deleteElement(EsperaTamaño);
+		// Step 4. Compare the expected versus actual values
+		int pos = PruebaTamaño.searchElementByValue(EsperaTamaño);
+
+		assertEquals(-1, pos);
 	}
 
+	
+
 }
+
