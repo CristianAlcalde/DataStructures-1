7780589b2ee92a89c48939f914fa427e98fa20cc
Alejandra Rivera <alejarivrin@hotmail.com>
2018-08-28 20:21:35 -0500

    Matrix.

--- src/cap1/arrays/Matrix.java
+++ src/cap1/arrays/Matrix.java
@@ -2,6 +2,8 @@ package cap1.arrays;
 
 public class Matrix {
 	// Step 1. Create attributes
+	
+	
 	// Step 1.1. Create Constructor
 	// Step 2. Create methods
 	// Step 2.1. Generate encapsulation methods
--- src/cap1/arrays/Vector.java
+++ src/cap1/arrays/Vector.java
@@ -69,3 +69,6 @@ public class Vector {
 		return this.elements;
 	}
 }
+
+
+
