18e337937aa1e0c97cb9dda347d8059109d8b052
cristian.alcalde <cristian.alcalde@utp.edu.co>
2018-08-29 21:24:51 -0500

    To create testSearch

--- src/test/MatrixTest.java
+++ src/test/MatrixTest.java
@@ -2,6 +2,7 @@ package test;
 
 import static org.junit.Assert.assertArrayEquals;
 import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.Assert.assertEquals;
 
 import org.junit.jupiter.api.Test;
 
@@ -14,12 +15,25 @@ class MatrixTest {
 		// Step 1. instantiate the tested class using the constructor method
 		Matrix testedMatrix = new Matrix(10,10);
 		// Step 2. Create the expected value variable
-		int[][] expectedValue = new int[1][1];
-		expectedValue[1][1] = 999;
+		int[][] expectedValue = new int[10][10];
+		expectedValue[0][0] = 999;
 		// Step 3. Obtain the actual value
-		int[][] actualValue = testedMatrix.insertElement(999, 1, 1);
+		int[][] actualValue = testedMatrix.insertElement(999, 0, 0);
 		// Step 4. Compare the expected versus actual values
 		assertArrayEquals(expectedValue, actualValue);
+	}
+	
+	@Test
+	void testSearchElement() {
+		// Step 1. instantiate the tested class using the constructor method
+		Matrix testedMatrix = new Matrix(10,10);
+		// Step 2. Create the expected value variable
+		int expectedValue = 999;
+		testedMatrix.insertElement(expectedValue, 9, 9);
+		// Step 3. Obtain the actual value
+		int actualValue = testedMatrix.searchElementByPosition(9,9);
+		// Step 4. Compare the expected versus actual values
+		assertEquals(expectedValue, actualValue);
 	}
 
 }
