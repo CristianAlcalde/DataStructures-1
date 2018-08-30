f08629bb3b8bc33ff0893cb0929bc1cc1c0a3230
Maicol <maiikol_96@utp.edu.co>
2018-08-30 14:00:15 -0500

    corrigiendo error en el repositorio y re-subiendo la matriz con sus respectivos tests

--- src/cap1/arrays/Matrix.java
+++ src/cap1/arrays/Matrix.java
@@ -8,10 +8,11 @@ public class Matrix {
 	
 	// Step 1.1. Create Constructor
 	
-	public Matrix(int newFilas, int NewColumnas) {
-		this.filas = newFilas;
-		this.columnas = newColumnas
+	public Matrix(int newFilas, int newColumnas) {
+		this.filas= newFilas;
+		this.columnas= newColumnas;
 		this.elementos = new int[newFilas][newColumnas];
+				}
 	
 	// Step 2. Create methods
 	// Step 2.1. Generate encapsulation methods	
@@ -42,7 +43,38 @@ public class Matrix {
 	
     // Step 2.2. Create CRUD methods
 
-     public int[][] insertarElementos(int[][] elementos, int fila, int columna ) {
- 		this.elementos[fila][columna] = elementos[][];
- 		return this.elementos[][] ;
+     public int[][] insertarElementos(int elementos, int filas, int columnas ) {
+ 		this.filas=filas;
+ 		this.columnas=columnas;
+ 		this.elementos[filas][columnas]=elementos;
+ 				return this.elementos;
  	}
+     
+     public int buscarElemento(int elemento) {
+ 		for (int filas=0; filas<this.filas; filas++);
+ 		for (int columnas=0; columnas<this.columnas; columnas++) {
+ 		if (this.elementos[filas][columnas]==elemento)
+ 			System.out.println(filas + "," + columnas);
+ 	}
+ 		return elemento; 
+}
+   
+     public int[][] actualizarElemento(int elemento, int fila, int columna) {
+ 		this.elementos[fila][columna] = elemento;
+ 		return this.elementos;
+ 	}
+     
+     public int[][] eliminarElemento(int elemento) {
+ 		
+ 		for(int i=0; i<this.elementos.length; i++) {
+ 			for (int j = 0; j < elementos.length; j++) {
+ 				
+ 				if (elementos[i][j] == elemento) {
+ 					elementos[i][j] = 0;
+ 				}
+ 			}
+ 		}
+ 		return this.elementos;
+ 	}
+
+}
--- src/test/MatrixTest.java
+++ src/test/MatrixTest.java
@@ -4,41 +4,69 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
 
+import cap1.arrays.Matrix;
+
 class MatrixTest {
 
 	@Test
-	void test() {
-		
-		void testInsertElement() {
-			// Step 1. instantiate the tested class using the constructor method
-			Matrix testedMatrix = new Matrix[10][10];
-			// Step 2. Create the expected value variable
-			int[][] expectedValue = new int[10][10];
-			expectedValue[0][0] = 999;
-			// Step 3. Obtain the actual value
-			int[][] actualValue = testedMatrix.insertElement(999, [0][0]);
-			// Step 4. Compare the expected versus actual values
-			assertArrayEquals(expectedValue, actualValue);
-		}
-		
-		@Test
-		void testSearchElement() {
-			// Step 1. instantiate the tested class using the constructor method
-			Matrix testedMatrix = new Matrix[10][10];
-			// Step 2. Create the expected value variable
-			int expectedValue = 999;
-			testedMatrix.insertElement(expectedValue, 9);
-			// Step 3. Obtain the actual value
-			int actualValue = testedVector.buscarElemento(9);
-			// Step 4. Compare the expected versus actual values
-			assertEquals(expectedValue, actualValue);
-		
+	void testInsertarElementos() {
+		// Step 1. instantiate the tested class using the constructor method
+		Matrix testedMatrix = new Matrix(5,5);
+		// Step 2. Create the expected value variable
+		int[][] expectedValue = new int[5][5];
+		expectedValue[2][2] = 10;
+		// Step 3. Obtain the actual value
+		int[][] actualValue = testedMatrix.insertarElementos(10, 2, 2);
+		// Step 4. Compare the expected versus actual values
+		assertArrayEquals(expectedValue, actualValue);
+	}
+	
+    @Test
+	void testBuscarElemento() {
+		// Step 1. instantiate the tested class using the constructor method
+		Matrix testedMatrix = new Matrix(5,5);
+		// Step 2. Create the expected value variable
+		int[][] expectedValue = new int[5][5];
+		expectedValue[2][2] = 10;
+		// Step 3. Obtain the actual value
+		int[][] actualValue = testedMatrix.insertarElementos(10, 2, 2);
+		// Step 4. Compare the expected versus actual values
+		assertArrayEquals(expectedValue, actualValue);
+	}
 		
 		
 		
-		
-		
-		
+   
+	@Test
+	void testEliminarElemento() {
+		// Step 1. instantiate the tested class using the constructor method
+		Matrix testedMatrix = new Matrix(5,5);
+		// Step 2. Create the expected value variable
+		int expectedValue = 10;
+		testedMatrix.insertarElementos(expectedValue, 2, 2);
+		// Step 3. Obtain the actual value
+		int[][] actualValue = testedMatrix.eliminarElemento(expectedValue);
+		// Step 4. Compare the expected versus actual values
+		int elemento = testedMatrix.buscarElemento(expectedValue);
+		assertEquals(10, elemento);
 	}
 
-}
+
+	@Test
+	void testActualizarElemento() {
+		// Step 1. instantiate the tested class using the constructor method
+		Matrix testedMatrix = new Matrix(5, 5);
+		// Step 2. Create the expected value variable
+		int[][] expectedValue = new int[5][5];
+		expectedValue[2][2] = 50;
+		// Step 3. Obtain the actual value
+		testedMatrix.insertarElementos(50, 2, 2);
+		int[][] actualValue = testedMatrix.actualizarElemento(50, 2, 2);
+		// Step 4. Compare the expected versus actual values
+		assertArrayEquals(expectedValue, actualValue);
+	}
+
+}	
+		
+	
+
