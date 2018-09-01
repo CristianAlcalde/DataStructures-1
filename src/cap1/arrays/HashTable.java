6461a2908d096c9aaf4eba2fb5d8ecfc824635d3
Vladimir <v.castaneda@utp.edu.co>
2018-08-31 21:27:57 -0500

    Empezando HasthTable

---
+++ src/cap1/arrays/HashTable.java
@@ -0,0 +1,35 @@
+package cap1.arrays;
+
+public class HashTable {
+	
+	// Step 1. Create attributes
+	Person[] elements;
+	float changeFactor;
+	int size;
+	
+	// Step 2. Create methods
+	 // Step 2.1. Generate encapsulation methods
+	
+	public Person[] getElements() {
+		return elements;
+	}
+	public void setElements(Person[] elements) {
+		this.elements = elements;
+	}
+	public float getChangeFactor() {
+		return changeFactor;
+	}
+	public void setChangeFactor(float changeFactor) {
+		this.changeFactor = changeFactor;
+	}
+	public int getSize() {
+		return size;
+	}
+	public void setSize(int size) {
+		this.size = size;
+	}
+	
+	
+	
+
+}
