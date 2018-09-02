59df3c9453d044741c610c6bfd48eeea05653f76
alejandroSoto <alejandro.soto@utp.edu.co>
2018-09-01 20:25:22 -0500

    terminando de crear hashFunction

--- src/cap1/arrays/HashTable.java
+++ src/cap1/arrays/HashTable.java
@@ -1,92 +1,86 @@
 package cap1.arrays;
 
 public class HashTable {
-	
+
 	// Step 1. Create attributes
 	private Person[] elements;
 	private float chargeFactor;
 	private int size;
-	private String[] vec;
-	
+
 	// Step 1.1. Create Constructor
 	public HashTable(int newSize) {
 		System.out.println("Creating vector with size: " + newSize);
 		this.size = newSize;
 		this.elements = new Person[newSize];
-		this.vec = new String[newSize/2+1];
-		
+
 	}
-	
+
 	// Step 2. Generate encapsulation methods
 	public Person[] getElements() {
 		return elements;
 	}
+
 	public void setElements(Person[] elements) {
 		this.elements = elements;
 	}
-	
+
 	public float getChargeFactor() {
 		return chargeFactor;
 	}
+
 	public void setChargeFactor(float chargeFactor) {
 		this.chargeFactor = chargeFactor;
 	}
-	
+
 	public int getSize() {
 		return size;
 	}
+
 	public void setSize(int size) {
 		this.size = size;
 	}
-		
-	public String[] getVec() {
-		return vec;
-	}
 
-	public void setVec(String[] vec) {
-		this.vec = vec;
-	}
-
-	// metodo para hallar el indice Hash
-	public Integer convert(Person intDocument) {
-		
-		int newSize=intDocument.getDocument().length();
-		String[] vec = new String[newSize/2+1];
-
-		int pos=0;
-		for(int i=0; i<=intDocument.getDocument().length()-2; i+=2) {
-			String separate = intDocument.getDocument().substring(i, i+2);
-			vec[pos]=separate;
-			pos=pos+1;
-			vec[pos]=" ";
-		}
-		if(intDocument.getDocument().length()%2==1) {
-			vec[pos]=intDocument.getDocument().substring(intDocument.getDocument().length()-1, intDocument.getDocument().length());
-		}
-		
-				
-		
-		
-		return 0;
-		
-		
-	}
 	
-//	public int hashFunction(Person intDocument) {
-		
-		
+	// metodo para hallar el indice Hash
+	public Integer hashFunction(Person intDocument) {
+
+		String code = intDocument.getDocument();
+		Integer[] vec = new Integer[code.length() / 2 + 1];
+		int suma = 0;
+		int pos = 0;
+
+		if (code.length() % 2 == 0) {
+			for (int i = 0; i <= code.length() - 2; i += 2) {
+				String separate = code.substring(i, i + 2);
+				vec[pos] = Integer.parseInt(separate);
+				pos = pos + 1;
+				vec[pos] = 0;
+
+			}
+		} else {
+			for (int i = 0; i <= code.length() - 2; i += 2) {
+				String separate = code.substring(i, i + 2);
+				vec[pos] = Integer.parseInt(separate);
+				pos = pos + 1;
+				vec[pos] = Integer.parseInt(code.substring(code.length() - 1, code.length()));
+			}
+		}
+
+		for (int i = 0; i < vec.length; i++) {
+			suma = suma + vec[i];
+		}
+
+		return 0;
+
+	}
+
 //	}
 //		intDocument.setDocument()
-			
+
 //	}
-	
+
 	// Step 3. Create CRUD methods
-	
-	
-	
+
 	// Step 3.1 Creating Insert methods
-	
-	
-	
 
 }
