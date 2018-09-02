7b14d42fd59e83560d32ddaac41db32e72440428
Wolpretrex <alejandro.vallejo@utp.edu.co>
2018-09-02 16:30:30 -0500

    Se agrega el constructor y el encapsulamiento

--- src/cap1/arrays/HashTable.java
+++ src/cap1/arrays/HashTable.java
@@ -1,8 +1,38 @@
 package cap1.arrays;
 
 public class HashTable {
-	
+	private int[] person;
+	private int changefactor;
 	private int size;
-	private
+
+	// Step 1. Create attributes
+	
+	public int getSize() {
+		return size;
+	}
+	public void setSize(int size) {
+		this.size = size;
+	}
+	public int[] getPerson() {
+		return person;
+	}
+	public void setPerson(int[] person) {
+		this.person = person;
+	}
+	public int getChangefactor() {
+		return changefactor;
+	}
+	public void setChangefactor(int changefactor) {
+		this.changefactor = changefactor;
+	}
+	
+	// Step 1.1. Create Constructor
+	
+	public HashTable(int newSize) 
+	{
+		System.out.println("The Size the HashTable is: "+newSize);
+		this.size = newSize;
+		this.person = new int[newSize];
+	} 
 
 }
