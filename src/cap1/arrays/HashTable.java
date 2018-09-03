dba347ec72efa23bad2e6d031f1e7a65875294fc
Wolpretrex <alejandro.vallejo@utp.edu.co>
2018-09-02 23:15:41 -0500

    Se agrega la funcion de hash para poder separar el numero, y se agrega la opcionde digitar cedula por teclado para facilitar por el momento la impresion de esta, parqa probar algunas caracteristicaz.

--- src/cap1/arrays/HashTable.java
+++ src/cap1/arrays/HashTable.java
@@ -1,9 +1,16 @@
 package cap1.arrays;
 
+import java.util.Scanner;
+
 public class HashTable {
-	private int[] person;
-	private int changefactor;
+	private Person[] elements;
+	private float chargefactor;
 	private int size;
+	private Integer num;
+	private static String Documento;
+	
+
+	
 
 	// Step 1. Create attributes
 	
@@ -13,26 +20,80 @@ public class HashTable {
 	public void setSize(int size) {
 		this.size = size;
 	}
-	public int[] getPerson() {
-		return person;
-	}
-	public void setPerson(int[] person) {
-		this.person = person;
-	}
-	public int getChangefactor() {
-		return changefactor;
-	}
-	public void setChangefactor(int changefactor) {
-		this.changefactor = changefactor;
-	}
+	
+	
 	
 	// Step 1.1. Create Constructor
 	
+	public Person[] getElements() {
+		return elements;
+	}
+	public void setElements(Person[] elements) {
+		this.elements = elements;
+	}
+	public float getChargefactor() {
+		return chargefactor;
+	}
+	public void setChargefactor(float chargefactor) {
+		this.chargefactor = chargefactor;
+	}
+	public Integer getNum() {
+		return num;
+	}
+	public void setNum(Integer num) {
+		this.num = num;
+	}
+	public String getDocumento() {
+		return Documento;
+	}
+	public void setDocumento(String documento) {
+		Documento = documento;
+	}
 	public HashTable(int newSize) 
 	{
 		System.out.println("The Size the HashTable is: "+newSize);
 		this.size = newSize;
-		this.person = new int[newSize];
+		this.elements = new Person[newSize];
 	} 
+	
+	
+	// Step 2.2. Create CRUD methods
+	
+	
+	
+	public static void main(String[] args) {
+		
+	
+		HashTable Ejecutar = new HashTable(13);
+	    Scanner entradaEscaner = new Scanner (System.in); 
+		
+		System.out.println("Digite el documento: ");
+		Documento = entradaEscaner.nextLine();
+		
+		
+		Integer suma=0;			
+		String deCerooaUno = "";
+		String deDosaTres = "";
+		String deCuatroaCinco= "";
+		String deSeisaSiete = "";
+		String deOchoaNueve = "";
+		int cont = 0;
+		
+		for(int i = 0; i<=20;i= i+2){
+			//if(Ejecutar.Documento.length<10)
+			deCerooaUno = "" + Ejecutar.Documento.charAt(i) + Ejecutar.Documento.charAt(i+1);
+			System.out.println(deCerooaUno);
+		}
 
+		System.out.println(deDosaTres);
+		System.out.println(deCuatroaCinco);
+		System.out.println(deSeisaSiete);
+		System.out.println(deOchoaNueve);
+		
+		
+		System.out.println(suma);
+		//System.out.println(suma % 13);
+	}
+
+	
 }
