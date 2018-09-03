423881eb30e88995f1367407a8030077c4109d69
jdelgadoutp <j.delgado@utp.edu.co>
2018-09-02 20:57:19 -0500

    Se crea funcionhash.

--- src/cap1/arrays/HashTable.java
+++ src/cap1/arrays/HashTable.java
@@ -1,5 +1,64 @@
 package cap1.arrays;
 
+import java.util.StringTokenizer;
+
 public class HashTable {
+
+	// Step 1. Create attributes
+
+	private Person[] elements;
+	private float chargefactor;
+	private int size;
+
+	// Step 1.1. Create Constructor
+
+	public HashTable(int newsize) {
+		this.size = newsize;
+		this.chargefactor = 0;
+		this.elements = new Person[newsize];
+	}
+
+	// Step 2. Create methods
+	// Step 2.1. Generate encapsulation methods
+
+	public Person[] getElements() {
+		return elements;
+	}
+
+	public void setElements(Person[] elements) {
+		this.elements = elements;
+	}
+
+	public float getChangefactor() {
+		return chargefactor;
+	}
+
+	public void setChangefactor(float changefactor) {
+		this.chargefactor = changefactor;
+	}
+
+	public int getSize() {
+		return size;
+	}
+
+	public void setSize(int size) {
+		this.size = size;
+	}
+
+	// Step 2.2. Create CRUD methods
+
+	public int FuntionHash(String document) {
+		
+		Integer par1 = (Integer.parseInt(document))%100;
+		Integer par2 = (Integer.parseInt(document)) / 100 % 100;
+		Integer par3 = (Integer.parseInt(document)) / 100 / 100 % 100;
+		Integer par4 = (Integer.parseInt(document)) / 100 / 100 / 100 % 100;
+		Integer par5 = (Integer.parseInt(document)) / 100 / 100 / 100 /100 % 100;
+		Integer par6 = (Integer.parseInt(document)) / 100 / 100 / 100 /100 /100 % 100;
+		Integer par7 = (Integer.parseInt(document)) / 100 / 100 / 100 /100 /100 % 100;
+		
+		return ((par1+par2+par3+par4+par5+par6+par7) % this.size);
+
+	}
 
 }
--- src/test/HashTableTest.java
+++ src/test/HashTableTest.java
@@ -1,14 +1,24 @@
 package test;
 
+import static org.junit.Assert.assertArrayEquals;
 import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
 
+import cap1.arrays.HashTable;
+import cap1.arrays.Vector;
+
 class HashTableTest {
 
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	void FuntionHash() {
+		// Step 1. instantiate the tested class using the constructor method
+		HashTable testedHash = new HashTable(13);
+		// Step 2. Create the expected value variable
+		int expectedValue = 12;
+		// Step 3. Obtain the actual value
+		int actualValue = testedHash.FuntionHash("18609698");
+		// Step 4. Compare the expected versus actual values
+		assertEquals(expectedValue, actualValue);
 	}
-
 }
