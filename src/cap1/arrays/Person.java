fa614b9c10e4fd5659e3bed55e599273497995b3
Maicol <maiikol_96@utp.edu.co>
2018-09-02 23:30:45 -0500

    Creando las clases Person y HashTable y creando sus respectivos metodos encapsulados

---
+++ src/cap1/arrays/Person.java
@@ -0,0 +1,36 @@
+package cap1.arrays;
+
+public class Person {
+
+	// Step 1. Create attributes
+	
+		private String document;
+		private String name;
+		private String phone;
+		
+		
+	// Step 2. Create methods
+	// Step 2.1. Generate encapsulation methods	
+		
+		public String getDocument() {
+			return document;
+		}
+		public void setDocument(String document) {
+			this.document = document;
+		}
+		public String getName() {
+			return name;
+		}
+		public void setName(String name) {
+			this.name = name;
+		}
+		public String getPhone() {
+			return phone;
+		}
+		public void setPhone(String phone) {
+			this.phone = phone;
+		}
+		
+		
+	 // Step 2.2. Create CRUD methods
+}
