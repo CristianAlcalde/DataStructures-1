09b0eaea926f7d748fcf856cc5e60c1d63e65f3a
JuanCa958808LDS <juanca8@utp.edu.co>
2018-09-03 19:44:58 -0500

    Person inicio

---
+++ Person/Person.java
@@ -0,0 +1,50 @@
+
+package Person;
+import java.util.*;
+
+public class Person {
+
+private String name;
+private String document;
+private String phone;
+
+public String getPhone() {
+	return phone;
+}
+public void setPhone(String phone) {
+	this.phone = phone;
+}
+public String getName() {
+	return name;
+}
+public void setName(String name) {
+	this.name = name;
+}
+public String getDocument() {
+	return document;
+}
+public void setDocument(String document) {
+	this.document = document;
+}
+
+  public Person(String name,String document,String phone) {
+	
+	this.name = name;
+	
+	this.document = document;
+	
+	this.phone = phone;
+	
+	
+}
+	
+	
+	
+
+	
+	
+	
+	
+	
+	
+}
