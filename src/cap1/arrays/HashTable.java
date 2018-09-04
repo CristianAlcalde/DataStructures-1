4fa37c0e48383f98d8396ce690106f411aa45001
Giovany Quintero O <giovanyquintero1@hotmail.com>
2018-09-04 12:35:56 -0500

    Encapsulamiento

--- src/cap1/arrays/HashTable.java
+++ src/cap1/arrays/HashTable.java
@@ -1,5 +1,27 @@
 package cap1.arrays;
 
 public class HashTable {
-
+	String [] Person;
+	int size;
+	float changefactor;
+	public String[] getPerson() {
+		return Person;
+	}
+	public void setPerson(String[] person) {
+		Person = person;
+	}
+	public int getSize() {
+		return size;
+	}
+	public void setSize(int size) {
+		this.size = size;
+	}
+	public float getChangefactor() {
+		return changefactor;
+	}
+	public void setChangefactor(float changefactor) {
+		this.changefactor = changefactor;
+	}
+	
+	
 }
--- src/cap1/arrays/person.java
+++ src/cap1/arrays/person.java
@@ -1,5 +1,28 @@
 package cap1.arrays;
 
 public class person {
+	String name;
+	String document;
+	String phone;
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getDocument() {
+		return document;
+	}
+	public void setDocument(String document) {
+		this.document = document;
+	}
+	public String getPhone() {
+		return phone;
+	}
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+
 
 }
