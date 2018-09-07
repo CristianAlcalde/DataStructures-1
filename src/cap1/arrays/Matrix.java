043e9a4e595e461a75edeab83ee21709f292169b
CristianOspina <cristian.ospina1@utp.edu.co>
2018-09-06 19:38:36 -0500

    matriz

--- src/cap1/arrays/Matrix.java
+++ src/cap1/arrays/Matrix.java
@@ -6,4 +6,65 @@ public class Matrix {
 	// Step 2. Create methods
 	// Step 2.1. Generate encapsulation methods
 	// Step 2.2. Create CRUD methods
+	 private int fil;
+	  private int col;
+	  private int[][] matrix;
+	  
+	  public Matrix(int fil, int col) { 
+		this.fil = fil;
+	    this.col = col;
+	    matrix = new int[fil][col];
+	  }
+	  
+	  public void agregar(int i, int a, int valor) {
+	    if ((i < fil) && (i >= 0) && (a < col) && (a >= 0))
+	      matrix[i][a] = valor;
+	  }
+	  
+	  public int eliminarElemento(int pos2, int pus) {
+	    int i = 0;int a = 0;
+	    System.out.println("eliminar algun elemento");
+	    return matrix[i][a] = 0;
+	  }
+	  
+	  public int listar()
+	  {
+	    for (int i = 0; i < fil; i++) {
+	      for (int j = 0; j < col; j++) {
+	        System.out.print(matrix[i][j] + " ");
+	      }
+	      System.out.println(" ");
+	    }
+	    return 0;
+	  }
+	  
+	  public int multi() {
+	    int mul = 0;
+	    for (int i = 0; i < fil; i++) {
+	      for (int j = 0; j < col; j++) {
+	        mul = i * j;
+	        System.out.print(mul);
+	      }
+	    }
+	    
+	    return mul;
+	  }
+	  
+	  public int buscar(int i, int a) {
+	    if ((i < fil) && (i >= 0) && (a < col) && (a >= 0)) {
+	      System.out.print(matrix[i][a] + " ");
+	      return matrix[i][a];
+	    }
+	    
+	    return -1;
+	  }
+	  
+	  public int actualizar(int i, int a, int valor)
+	  {
+	    if ((i < fil) && (i >= 0) && (a < col) && (a >= 0)) {
+	      matrix[i][a] = valor;
+	    }
+	    return valor;
+	  }
 }
+
--- src/test/MatrixTest.java
+++ src/test/MatrixTest.java
@@ -1,14 +1,75 @@
 package test;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 import org.junit.jupiter.api.Test;
+
+import cap1.arrays.Matrix;
 
 class MatrixTest {
 
-	@Test
-	void test() {
-		fail("Not yet implemented");
-	}
+	public MatrixTest() {}
+	  
+	  Matrix matri = new Matrix(5, 5);
+	  
+	  @Test
+	  public void tesagregarbuscartMatriz() {
+	    matri.agregar(1, 0, 12);
+	    matri.agregar(1, 1, 10);
+	    matri.agregar(2, 2, 13);
+	    matri.agregar(2, 3, 14);
+	    matri.agregar(2, 4, 15);
+	    matri.agregar(2, 5, 16);
+	    int esperado = matri.buscar(1, 0);
+	    int valoresperado = matri.buscar(1, 0);
+	    org.junit.Assert.assertEquals(valoresperado, esperado);
+	  }
+	  
+	  @Test
+	  public void testEliminarElemento()
+	  {
+	    matri.agregar(1, 0, 12);
+	    matri.agregar(1, 1, 10);
+	    matri.agregar(2, 2, 13);
+	    matri.agregar(2, 3, 14);
+	    matri.agregar(2, 4, 15);
+	    matri.agregar(2, 5, 16);
+	    int dato = matri.eliminarElemento(1, 0);
+	    int esperado = matri.eliminarElemento(1, 0);
+	    org.junit.Assert.assertEquals(esperado, dato);
+	  }
+	  
+	  @Test
+	  public void testListar() {
+	    matri.agregar(0, 0, 12);
+	    matri.agregar(1, 1, 10);
+	    matri.agregar(2, 2, 13);
+	    matri.agregar(3, 3, 14);
+	    matri.agregar(4, 4, 15);
+	    matri.agregar(5, 5, 16);
+	    int dato = matri.listar();
+	    int esperado = matri.listar();
+	    org.junit.Assert.assertEquals(esperado, dato);
+	  }
+	  
+	  @Test
+	  public void testActualizar() {
+	    matri.agregar(0, 0, 12);
+	    int dato = matri.actualizar(0, 0, 10);
+	    int esperado = 10;
+	    org.junit.Assert.assertEquals(esperado, dato);
+	  }
+	  
+
+	  @Test
+	  public void testmulti()
+	  {
+	    matri.agregar(0, 0, 12);
+	    matri.agregar(0, 0, 2);
+	    
+	    int dato = matri.multi();
+	    int esperado = matri.multi();
+	    org.junit.Assert.assertEquals(esperado, dato);
+	  }
+
+			
 
 }
