0ce8d25647eefd485b70c26ee253ae41169cddb5
JuanCa958808LDS <juanca8@utp.edu.co>
2018-09-10 11:13:24 -0500

    Add files via upload

--- Matriz/Matriz/src/test/MatrizTest.java
+++ Matriz/Matriz/src/test/MatrizTest.java
@@ -1,8 +1,8 @@
 package test;
 
-//import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
 
-//import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Test;
 
 class MatrizTest {
 	private int numeroFilas;
