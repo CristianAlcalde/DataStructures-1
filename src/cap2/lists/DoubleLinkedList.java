e545b2a758b407c56402822056c9c9828f448f74
alejandroSoto <alejandro.soto@utp.edu.co>
2018-09-16 23:45:07 -0500

    Metodo addOrdered en DoubleLinkedList

--- src/cap2/lists/DoubleLinkedList.java
+++ src/cap2/lists/DoubleLinkedList.java
@@ -5,7 +5,7 @@ public class DoubleLinkedList {
 	private DoubleLinkedNode head; // Cabeza
 	private DoubleLinkedNode tail; // Cola
 	private int size = 0;
-	
+		
 	// Encapsulate the required fields
 	public int getSize() {
 		return size;
@@ -27,8 +27,18 @@ public class DoubleLinkedList {
 	}
 	
 	// CRUD
-	public void add(String author, String file, String name, String trackNo) {
-		
+	public void change(DoubleLinkedNode node, DoubleLinkedNode newNode) {
+		DoubleLinkedNode nodeTemp = new DoubleLinkedNode();	
+		if(Integer.parseInt(node.getDato().getTrackNo())>Integer.parseInt(newNode.getDato().getTrackNo())) {
+			nodeTemp = node;
+			node = newNode;
+			newNode = nodeTemp;
+		}
+	}
+	
+	public void addOrdered(String author, String file, String name, String trackNo) {
+		DoubleLinkedNode compare1;
+		DoubleLinkedNode compare2;
 		// Step 1. create the new node
 		DoubleLinkedNode newNode = new DoubleLinkedNode(author, file, name, trackNo);
 		// If the head is null
@@ -37,11 +47,32 @@ public class DoubleLinkedList {
 			this.tail = newNode;
 			
 		} else {
-			this.tail.setNext(newNode);
-			newNode.setPrevious(tail);
-			this.tail = newNode;
+			compare1 = head;
+			while(compare1 != null) {
+				
+				compare2 = compare1.getNext();
+				if(Integer.parseInt(newNode.getDato().getTrackNo())<=Integer.parseInt(compare1.getDato().getTrackNo())) {
+					newNode.setNext(head);
+					this.head = newNode;
+				} else {
+					if(Integer.parseInt(newNode.getDato().getTrackNo())>Integer.parseInt(compare1.getDato().getTrackNo()) && compare2 == null){
+						compare1.setNext(newNode);
+						this.tail = newNode;
+					} else {
+						if(Integer.parseInt(compare1.getDato().getTrackNo())<Integer.parseInt(newNode.getDato().getTrackNo()) && Integer.parseInt(compare2.getDato().getTrackNo())>=Integer.parseInt(newNode.getDato().getTrackNo())){
+							compare1.setNext(newNode);
+							newNode.setNext(compare2);
+						}
+						else {
+							compare1 = compare1.getNext();
+						}
+					}
+				}
+				
+			}
+	
 		}
-		this.size = this.size + 1;
+		this.size = this.size + 1;	
 	}
 
 }
